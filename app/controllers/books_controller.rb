4ecaf2a3cd88b12da09db368a5cc3de1792af1f8
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-03-01 17:28:32 +0000

    [Finish]デバックの終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,16 @@
 class BooksController < ApplicationController
+  
+  before_action :ensure_correct_book, only: [:update, :edit]
 
   def show
     @book = Book.find(params[:id])
+    @user = User.find_by(id: @book.user_id)
+    @new_book = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -34,16 +39,23 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body, :profile_image)
+  end
+  
+  def ensure_correct_book
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 
 end
